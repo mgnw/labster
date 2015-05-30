557daecda9afa14a8c98f6af780be193f183882b
mgnw <csa2357@gmail.com>
2015-05-30 10:56:35 +0800

    Update

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Labster | Award Winning Virtual Laboratorium</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand"><img src="img/logo_03.png" class="logo"></a>
+				</div>
+				<div id="navbarCollapse" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="#">Course Catalog</a></li>
+						<li><a href="#">Labster.com</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="head-page">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="page-header">
+							<h1>Virtual laboratories for High School and College</h1>
+							<p class="lead"><small>HPLC &middot; Fermentation &middot; Medical Genetics &middot; Molecular Cloning &middot; Chromatography &middot; Enzyme Kinetics &middot; Cytogenetics &middot; Next Generation Sequencing</small></p>
+						</div>
+						<div class="col-md-offset-1 col-md-10 col-md-offset-1">
+							<div class="embed-responsive embed-responsive-16by9">
+								<iframe src="https://www.youtube.com/embed/oairUMkieak" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wide-page wide-page-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 col-md-offset-1">
+						<h1>Study Results</h1>
+						<p class="lead" style="padding-top:5px;">
+							Increase learning outcomes by more than 70%* <br/>
+							Improved student motivation and engagement<br/>
+							Real-world scenarios<br/>
+							Inquiry-based learning<br/>
+							Lower costs<br/>
+							<em><small>*Nature Biotechnology 32, 694–697 (2014) Improving biotech education through gamified laboratory simulations</small></em>
+						</p>
+					</div>
+					<div class="col-md-5">
+						<h1>88</h1>
+						<p class="lead">Out of 100 students say Labster can be a good supplement to regular teaching.</p>
+						<h1>>101%</h1>
+						<p class="lead">Higher learning achieved when combining Labster and lectures.</p>
+						<button class="btn btn-warning">Read more about the results of study &raquo;</button><br/><br/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-5 col-md-offset-1">
+						<h1>Free Online Demo</h1>
+						<p class="lead">
+							All Educators Can Book a Free Online Demo and Get Access to All Labs.
+						</p>
+						<button class="btn btn-warning">Get 30 Days Trial &raquo;</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="sep"></div>
+			<div class="row text-center">
+				<div class="col-md-4">
+					<img src="img/ico/round-1.png" class="head-img">
+					<h4>Teachers</h4>
+					<p class="lead">Appeal to various learning styles & make curriculum planning more efficient</p>
+					<p>
+						Structured for accelerated and integrated learning, Labster’s innovation lies in its  ability to fascinate students with story-based cases, outstanding design, and the unlimited freedom to explore the full lab experience.
+					</p>
+					<br/><button class="btn btn-default">Read More</button>
+					<div class="mobile-sep"></div>
+				</div>
+				<div class="col-md-4">
+					<img src="img/ico/round-2.png" class="head-img">
+					<h4>Administrators</h4>
+					<p class="lead">Save resources, support instructors & stay current</p>
+					<p>
+						Labster is a pioneering teaching tool that allows students to put theory into practice before they ever step into a wet-lab, saving schools thousands of dollars while optimizing the learning experience.
+					</p>
+					<br/><button class="btn btn-default">Read More</button>
+					<div class="mobile-sep"></div>
+				</div>
+				<div class="col-md-4">
+					<img src="img/ico/round-3.png" class="head-img">
+					<h4>Students</h4>
+					<p class="lead">Improve Theory Comprehension, Keep focused & Have Fun</p>
+					<p>
+						Students can spill reagents, mix combustible materials and enjoy limitless lab time! Whether they are reconstructing an ancient caveman or modifying yeast genetics, Labster gives them the flexibility to experiment and learn at their own pace.
+					</p>
+					<button class="btn btn-default">Read More</button>
+					<div class="mobile-sep"></div>
+				</div>
+			</div>
+			<div class="sep"></div>
+			<div class="row text-center">
+				<div class="col-md-12" style="padding-bottom: 40px;">
+					<h1>As Seen In</h1>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<img src="img/small-thumb-1.png" class="img-pad"><br/>
+					<a href="#">Labster – Helping Students Learn By Turning Science Class Into A Game</a>
+					<p>14 June 2014</p>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<img src="img/small-thumb-2.png" class="img-pad"><br/>
+					<a href="#">Improving Biotech Education Through Gamified Laboratory Simulations</a>
+					<p>14 June 2014</p>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<img src="img/small-thumb-3.png" class="img-pad"><br/>
+					<a href="#">Labster Gamifies Bio-Tech Teaching So Students Can Use Their iPads To Blow Stuff Up</a>
+					<p>14 June 2014</p>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<img src="img/small-thumb-4.png" class="img-pad"><br/>
+					<a href="">Empowering the Next Generation of Scientists to Save the World</a>
+					<p>14 June 2014</p>
+				</div>
+			</div>
+			<div class="sep"></div>
+			<div class="row text-center">
+				<div class="col-md-12">
+					<h1 class="text-center">Our Costumers</h1><br/><br/>
+					<img src="img/ico/com-1.png" class="company-img">
+					<img src="img/ico/com-2.png" class="company-img">
+					<img src="img/ico/com-1.png" class="company-img">
+					<img src="img/ico/com-2.png" class="company-img">
+					<img src="img/ico/com-1.png" class="company-img">
+				</div>
+			</div>
+			<div class="sep"></div>
+		</div>
+		<div class="wide-page wide-page-blue">
+			<footer class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2 col-md-offset-2 col-xs-6">
+							<h4 class="footer-heading">OUR LABS</h4>
+							<ul>
+								<li><a href="">Features</a></li>
+								<li><a href="">Course Catalog</a></li>
+								<li><a href="">Testimonial</a></li>
+							</ul>
+						</div>
+						<div class="col-md-2 col-xs-6">
+							<h4 class="footer-heading">ABOUT US</h4>
+							<ul>
+								<li><a href="">About Us</a></li>
+								<li><a href="">Press</a></li>
+								<li><a href="">Support</a></li>
+							</ul>
+						</div>
+						<div class="col-md-2 col-xs-6">
+							<h4 class="footer-heading">JOIN US</h4>
+							<ul>
+								<li><a href="">Careers</a></li>
+								<li><a href="">Ambassador Program</a></li>
+							</ul>
+						</div>
+						<div class="col-md-2 col-xs-6">
+							<h4 class="footer-heading">CONTACT US</h4>
+							<p>
+								Interesting in trying out our virtual labs?<br/>
+								Call +45 61437244 now or <a href="" class="contact">Contact Us</a><br/><br/>
+								<a href="#"><img src="img/ico/social-1.png" class="social-img"></a>
+								<a href="#"><img src="img/ico/social-2.png" class="social-img"></a>
+								<a href="#"><img src="img/ico/social-3.png" class="social-img"></a>
+								<a href="#"><img src="img/ico/social-4.png" class="social-img"></a>
+							</p>
+						</div>
+					</div>
+					<div class="row text-center sticky-foot">
+						<div class="container">
+							LABSTER <small>&copy; 2014 Labster ApS CVR:34457808 &middot; Terms and Conditions &middot; Privacy Policy</small>
+						</div>
+					</div>
+				</div>
+				
+			</footer>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
